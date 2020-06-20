fd69a29a5b89fcc37b8241930a12bcf31ec10055
Aloxaf <aloxafx@gmail.com>
2020-06-20 22:24:57 +0800

    Move code to upstream

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 ∠( ᐛ 」∠)＿
 
 Address: https://repo.aloxaf.com
+
 GPG Key: 016DB0252B1B1F11
+
--- packages/glibc/build.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-git clone https://git.archlinux.org/svntogit/packages.git/ -b packages/glibc --single-branch /tmp/glibc
-sed -i 's#!strip#debug#' /tmp/glibc/repos/core-x86_64/PKGBUILD
-echo Y | pikaur -P -k --noedit --mflags=--skipchecksums,--nocheck,--skippgpcheck /tmp/glibc/repos/core-x86_64/PKGBUILD
-cp ~/.cache/pikaur/build/glibc/*.xz ~/.cache/pikaur/pkg/

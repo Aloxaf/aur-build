eacb9b8f7780a9c9317e780f1f806ea63f1833cd
Aloxaf <aloxafx@gmail.com>
2020-06-23 22:30:16 +0800

    Better update strategy

--- script/packages/glibc/build.zsh
+++ script/packages/glibc/build.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
+local ret=0
 git clone https://git.archlinux.org/svntogit/packages.git/ -b packages/glibc --single-branch /tmp/glibc
 sed -i 's#!strip#debug#' /tmp/glibc/repos/core-x86_64/PKGBUILD
 echo Y | pikaur -P -k --mflags=--noprogressbar /tmp/glibc/repos/core-x86_64/PKGBUILD
+ret=$?
 cp ~/.cache/pikaur/build/glibc/*.pkg.tar.* ~/.cache/pikaur/pkg/
+return $ret
--- script/update_all.zsh
+++ script/update_all.zsh
@@ -6,42 +6,46 @@ PROJECT_ROOT=${0:A:h}
 mkdir -p ~/.cache/aur
 
 function need_update() {
-  # 不存在则需要升级
+  # 如果目录不存在，则需要构建
   if [[ ! -d $aur_dir ]]; then
     return 0
   fi
 
-  if [[ $1 == *-git || -f $aur_dir/build.zsh ]]; then
-    # -git 包或者自定义包 12 小时之后需要更新
-    if [[ ! -f $aur_dir/last_installed ]] ||
-         (( $EPOCHSECONDS - $(<$aur_dir/last_installed) >= 12 * 3600 )); then
-      return $?
-    else
-      return 1
-    fi
-  else
-    pushd $aur_dir
-    local ret=0
-    if [[ -d .git ]]; then
-      # 对于 AUR 包，检测上游是否更新
-      git remote update
-      [[ $(git rev-parse @) != $(git rev-parse @{u}) ]]
-      ret=$?
-    elif [[ -f PKGBUILD ]]; then
-      # 对于本地包，检测文件是否变化
-      local old=$(sha256sum *(.) | sed -E 's/ +.*//' | sha256sum)
-      local new=$(sha256sum $PROJECT_ROOT/packages/$1/*(.) | sed -E 's/ +.*//' | sha256sum)
-      [[ $old != $new ]]
-      ret=$?
-    fi
-    popd
-    return $ret
+  pushd $aur_dir
+  local ret=0
+  if [[ -d .git ]]; then
+    # 如果是 AUR 包，则检测上游是否更新
+    git remote update
+    [[ $(git rev-parse @) != $(git rev-parse @{u}) ]]
+    ret=$?
+  elif [[ -f PKGBUILD || -f build.zsh ]]; then
+    # 如果是本地包，则检测文件是否变化
+    local old=$(sha256sum *(.) | sed -E 's/ +.*//' | sha256sum)
+    local new=$(sha256sum $PROJECT_ROOT/packages/$1/*(.) | sed -E 's/ +.*//' | sha256sum)
+    [[ $old != $new ]]
+    ret=$?
   fi
+  popd
+  # 最后对 -git 包以及自定义构建脚本再检测一次
+  # 对于它们来说 12 小时之后强制更新一次
+  if (( $ret )); then
+    if [[ $1 == *-git || -f $aur_dir/build.zsh ]]; then
+      if [[ ! -f $aur_dir/last_installed ]] ||
+           (( $EPOCHSECONDS - $(<$aur_dir/last_installed) >= 12 * 3600 )); then
+        return 0
+      else
+        return 1
+      fi
+    fi
+  fi
+  return $ret
 }
 
 function build_packages() {
+  setopt local_options null_glob
   for package in $PROJECT_ROOT/packages/*; do
-    local aur_dir=~/.cache/aur/${package:t}
+    local aur_dir=~/.cache/aur/${package:t} ret=0
+    local -a packages=(~/.cache/pikaur/pkg/*)
 
     # 先检测是否需要更新，如果需要的话，直接删掉目录重建
     if ! need_update ${package:t}; then
@@ -54,7 +58,7 @@ function build_packages() {
       cp -r $package $aur_dir
       echo "Building ${package:t}"
       source $package/build.zsh
-      (( ! $? )) && echo -n $EPOCHSECONDS > $aur_dir/last_installed
+      ret=$?
     else
       if [[ ! -f $package/PKGBUILD ]]; then
         git clone https://aur.archlinux.org/${package:t}.git $aur_dir
@@ -63,7 +67,14 @@ function build_packages() {
       fi
       echo "Building ${package:t}"
       echo Y | pikaur -P --mflags=--noprogressbar $aur_dir/PKGBUILD
-      (( ! $? )) && echo -n $EPOCHSECONDS > $aur_dir/last_installed
+      ret=$?
+    fi
+    # 确认成功构建后更新时间戳
+    if (( ! $ret )); then
+      local -a new_packages=(~/.cache/pikaur/pkg/*)
+      if (( $#new_packages > $#packages )); then
+        echo -n $EPOCHSECONDS > $aur_dir/last_installed
+      fi
     fi
   done
 }
